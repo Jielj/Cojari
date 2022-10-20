8ebec11e5fd12f82494d76c608049d5ef46001bb
Jielj <87712774+Jielj@users.noreply.github.com>
2022-10-20 19:51:16 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   authenticate :user, ->(user) { user.admin? } do
