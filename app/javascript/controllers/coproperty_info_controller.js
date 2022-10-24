9cd93aa189f11df4bb92abadca0ffbf001fc1901
houssam chakir <thisischchannel@gmail.com>
2022-10-24 07:51:36 +0100

    progress on stimulus for property info listing

--- app/javascript/controllers/coproperty_info_controller.js
+++ app/javascript/controllers/coproperty_info_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="coproperty-info"
 export default class extends Controller {
   static values = {copId: String}
-  static targets = ["list"]
+  static targets = ["list", "cop_info_section"]
   connect() {
     console.log("property info js connected")
     console.log("the value : ", this.copIdValue)
@@ -14,13 +14,22 @@ export default class extends Controller {
   }
 
   selected() {
-    console.log("the value : ", )
+    // console.log("the value : ", )
 
-    fetch(`/getProperties?copId=${this.copIdValue}`, {headers: {"Accept": "text/plain"}})
-    .then(response => response.text())
-    .then((data) => {
-      this.listTarget.outerHTML = data
-    })
+    // fetch(`/getProperties?copId=${this.copIdValue}`, {headers: {"Accept": "text/plain"}})
+    // .then(response => response.text())
+    // .then((data) => {
+    //   this.listTarget.outerHTML = data
+    // })
+    console.log("item clicked")
+    console.info("cop info target: ",this.cop_info_sectionTarget)
+
+
+    if (this.codeIdValue == this.cop_info_sectionTarget) {
+      this.cop_info_sectionTarget.classList.remove("d-none")
+    } else {
+      this.cop_info_sectionTarget.classList.add("d-none")
+    }
 
   }
 }
