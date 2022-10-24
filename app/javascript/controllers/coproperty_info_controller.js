131e9319cf83227a7126f7cfb9266039ea8e14fe
houssam chakir <thisischchannel@gmail.com>
2022-10-24 21:21:10 +0100

    finished stimulus in coproperties page

--- app/javascript/controllers/coproperty_info_controller.js
+++ app/javascript/controllers/coproperty_info_controller.js
@@ -1,19 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="coproperty-info"
 export default class extends Controller {
-  static values = {copId: String}
-  static targets = ["list", "cop_info_section"]
+  static values = { copId: String };
+  static targets = ["list", "copInfoSection"];
   connect() {
-    console.log("property info js connected")
-    console.log("the value : ", this.copIdValue)
+    console.log("property info js connected");
   }
 
   highlight() {
     this.property_cardTarget.classList.add("highlight");
   }
 
-  selected() {
+  selected(event) {
     // console.log("the value : ", )
 
     // fetch(`/getProperties?copId=${this.copIdValue}`, {headers: {"Accept": "text/plain"}})
@@ -21,15 +20,19 @@ export default class extends Controller {
     // .then((data) => {
     //   this.listTarget.outerHTML = data
     // })
-    console.log("item clicked")
-    console.info("cop info target: ",this.cop_info_sectionTarget)
+    console.log("item clicked", event.target);
 
+    // console.info("cop info target: ", this.copInfoSectionTarget.getAttributes);
+    console.log("cop id value: ", this.copIdValue);
+    const properties = document.querySelectorAll(".propertiesList");
 
-    if (this.codeIdValue == this.cop_info_sectionTarget) {
-      this.cop_info_sectionTarget.classList.remove("d-none")
-    } else {
-      this.cop_info_sectionTarget.classList.add("d-none")
-    }
+    properties.forEach(p => {
+      p.classList.add("d-none");
+      console.log(this.copIdValue)
+      if (+this.copIdValue === +p.dataset.copId) {
+        p.classList.remove("d-none");
+      }
+    });
 
   }
 }
