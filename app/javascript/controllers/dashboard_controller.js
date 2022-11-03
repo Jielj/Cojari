c0e05ad0f65584eb403860853cde1fde8267f0bf
houssam chakir <thisischchannel@gmail.com>
2022-11-03 18:18:38 +0100

    making popup variables

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -5,11 +5,13 @@ export default class extends Controller {
   static values = {
     variable: String
   }
+  static targets = ["request"]
   connect() {
     console.log("dashboard js connected")
   }
 
   showVar() {
+    console.log(this.variableValue)
 
   }
 }
